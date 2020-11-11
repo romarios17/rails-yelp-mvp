ead0ccb84aa456f6bfe45536cdbb58e463516722
Romario Sanchez <romario.s17@protonmail.com>
2020-11-11 15:43:15 -0500

    done raking

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,23 +1,40 @@
 class RestaurantsController < ApplicationController
 
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find_by(id: params[:id])
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render  'new'
+    end
   end
 
   def update
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
   end
 
   def edit
   end
 
   def delete
+  end
+
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
